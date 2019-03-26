1abfb86fcdf0d8790941a7ba24581dafc2ef9435
elitejam <bteng@g.hmc.edu>
2019-03-26 10:56:16 -0700

    Updated Database with Wine props, added new db functions, and debug buttons

--- app/src/main/java/com/example/wineapp/DataManager.java
+++ app/src/main/java/com/example/wineapp/DataManager.java
@@ -6,29 +6,59 @@ import android.content.Context;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class DataManager {
     private SQLiteDatabase db;
     //    Row and Table Names used outside
-    public static final String TABLE_ROW_ID        = "_id";
-    public static final String TABLE_ROW_ID_DEF    = "integer primary key autoincrement not null";    
-    public static final String TABLE_ROW_NAME      = "name";
-    public static final String TABLE_ROW_NAME_DEF  = "text not null";    
-    public static final String TABLE_ROW_BRAND     = "age";
-    public static final String TABLE_ROW_BRAND_DEF = "text not null";    
+    public static final String TABLE_ROW_ID         = "_id";
+    public static final String TABLE_ROW_ID_DEF     = "integer primary key autoincrement not null";    
+    public static final String TABLE_ROW_NAME       = "name";
+    public static final String TABLE_ROW_NAME_DEF   = "text not null";    
+    public static final String TABLE_ROW_BRAND      = "brand";
+    public static final String TABLE_ROW_BRAND_DEF  = "text";
+    public static final String TABLE_ROW_RATING     = "rating";
+    public static final String TABLE_ROW_RATING_DEF = "real";
+    public static final String TABLE_ROW_COST       = "cost";
+    public static final String TABLE_ROW_COST_DEF   = "real";
+    public static final String TABLE_ROW_COLOR      = "color";
+    public static final String TABLE_ROW_COLOR_DEF  = "text";
 
     //    Row and Table used inside
-    private static final String DB_NAME = "address_book_db";
+    private static final String DB_NAME = "wine_list_db";
     private static final int DB_VERSION = 1;
-    private static final String TABLE_N_AND_A = "names_and_addresses";
+    private static final String TABLE_WINE = "wine_list";
+
+    public List<String> tableRowNames;
+    public List<String> tableRowProps;
 
 // ===================================================
 // Constructors
 // ===================================================
 
     public DataManager (Context context){
+
+        tableRowNames = new ArrayList<String>();
+        tableRowProps = new ArrayList<String>();
+        tableRowNames.add(TABLE_ROW_ID);
+        tableRowProps.add(TABLE_ROW_ID_DEF);
+        tableRowNames.add(TABLE_ROW_NAME);
+        tableRowProps.add(TABLE_ROW_NAME_DEF);
+        tableRowNames.add(TABLE_ROW_BRAND);
+        tableRowProps.add(TABLE_ROW_BRAND_DEF);
+        tableRowNames.add(TABLE_ROW_RATING);
+        tableRowProps.add(TABLE_ROW_RATING_DEF);
+        tableRowNames.add(TABLE_ROW_COST);
+        tableRowProps.add(TABLE_ROW_COST_DEF);
+        tableRowNames.add(TABLE_ROW_COLOR);
+        tableRowProps.add(TABLE_ROW_COLOR_DEF);
+        Log.i("Column size: ", Integer.toString(tableRowNames.size()));
+
 //        Create an instance of the SQLiteHelper
         CustomSQLiteOpenHelper helper = new CustomSQLiteOpenHelper(context);
         db = helper.getWritableDatabase();
+
     }
     private class CustomSQLiteOpenHelper extends SQLiteOpenHelper {
         public CustomSQLiteOpenHelper(Context context){
@@ -39,10 +69,17 @@ public class DataManager {
         public void onCreate(SQLiteDatabase db) {
             // Create a table
             String newTableQuery = "create table "
-                    + TABLE_N_AND_A   + " ( "
-                    + TABLE_ROW_ID    + " " + TABLE_ROW_ID_DEF    + ","
-                    + TABLE_ROW_NAME  + " " + TABLE_ROW_NAME_DEF  + ","
-                    + TABLE_ROW_BRAND + " " + TABLE_ROW_BRAND_DEF + ");";
+                    + TABLE_WINE   + " ( ";
+
+            String columns = "";
+
+            for(int i = 0; i < tableRowNames.size(); i++){
+                columns += tableRowNames.get(i) + " " + tableRowProps.get(i) + ",";
+            }
+            columns = columns.substring(0, columns.length()-1);
+            columns += ");";
+            newTableQuery += columns;
+            Log.i("Create Table: ", newTableQuery);
 
             db.execSQL(newTableQuery);
         }
@@ -55,7 +92,7 @@ public class DataManager {
 
     //    Insert into DB
     public void insert (String name, String age){
-        String query = "INSERT INTO " + TABLE_N_AND_A + " (" +
+        String query = "INSERT INTO " + TABLE_WINE + " (" +
                 TABLE_ROW_NAME + ", " +
                 TABLE_ROW_BRAND + " ) " +
                 "VALUES (" +
@@ -67,19 +104,42 @@ public class DataManager {
         // Execute command
         db.execSQL(query);
     }
+    //    Insert into DB
+    public void insert_flex (List<String> columnNames, List<String> contents){
+        String query = "INSERT INTO " + TABLE_WINE + " ( ";
+
+        String contentCols = "";
+        String contentVals = "";
+        for(int i = 0; i < columnNames.size(); i++){
+            contentCols += columnNames.get(i) + ", ";
+            contentVals += "'" + contents.get(i) + "'" + ", ";
+        }
+
+        query += contentCols.substring(0, contentCols.length()-1) + " ) "
+                    + "VALUES (" 
+                    + contentVals.substring(0, contentVals.length()-1) + ");";
+                    
+        // Print Statement
+        Log.i("insert() = ", query);
+
+        // Execute command
+        db.execSQL(query);
+    }
+
     //    Deletes an entry
     public void delete(String name){
-        String query = "DELETE FROM " + TABLE_N_AND_A +
+        String query = "DELETE FROM " + TABLE_WINE +
                 " WHERE " + TABLE_ROW_NAME +
                 " = '" + name + "';";
 
         Log.i("delete(); = ", query);
         db.execSQL(query);
     }
+
     //    Get all the records
     public Cursor selectAll() {
         Cursor c = db.rawQuery("SELECT *" + " from " +
-                TABLE_N_AND_A, null);
+                TABLE_WINE, null);
         return c;
     }
     //    Find a specific record
@@ -89,7 +149,7 @@ public class DataManager {
                 TABLE_ROW_NAME + ", " +
                 TABLE_ROW_BRAND  +
                 " from " +
-                TABLE_N_AND_A + " WHERE " +
+                TABLE_WINE + " WHERE " +
                 TABLE_ROW_NAME + " = '" + name + "';";
 
         Cursor c = db.rawQuery(query, null);
